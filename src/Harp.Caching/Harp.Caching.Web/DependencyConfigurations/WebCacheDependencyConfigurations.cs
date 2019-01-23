b1b17575d24adb88da91f5d2c3cfcbcddafd8762
Michael Grabchak <grabchak.michael@gmail.com>
2019-01-23 21:33:00 +0200

    Adding dependency configurations for caching implementations

---
+++ src/Harp.Caching/Harp.Caching.Web/DependencyConfigurations/WebCacheDependencyConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using Harp.Caching.DependencyConfigurations;
+using Harp.Dependencies;
+
+namespace Harp.Caching.Web.DependencyConfigurations
+{
+    public sealed class WebCacheDependencyConfigurations : CacheDependencyConfigurations
+    {
+        protected override void RegisterCacheContainer()
+        {
+            Container.RegisterType<ICache, WebCache>();
+        }
+    }
+}
---
+++ src/Harp.Caching/Harp.Caching/DependencyConfigurations/CacheDependencyConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Harp.Caching.Configurations;
+using Harp.Dependencies;
+using Harp.Dependencies.Configurations;
+
+namespace Harp.Caching.DependencyConfigurations
+{
+    public class CacheDependencyConfigurations : IDependencyConfiguration
+    {
+        public void Configure()
+        {
+            RegisterCacheConfig();
+            RegisterCacheContainer();
+        }
+
+        protected virtual void RegisterCacheConfig()
+        {
+            Container.RegisterType<ICacheConfiguration, CacheConfiguration>();
+        }
+
+        protected virtual void RegisterCacheContainer()
+        {
+            Container.RegisterType<ICache, NoCache>();
+        }
+    }
+}
